80df9bd5f33c4bd8d89cb8a4187772d18066e1f3
Walkers15 <qortmd7777@gmail.com>
2020-04-09 22:19:28 +0900

    API v1 추가_With JWT

--- 10장_nodebird-api/routes/middlewares.js
+++ 10장_nodebird-api/routes/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -11,5 +13,24 @@ exports.isNotLoggedIn = (req, res, next) => {
     next();
   } else {
     res.redirect('/');
+  }
+};
+
+exports.verifyToken = (req, res ,next) =>{
+  try{
+    req.decoded = jwt.verif(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch(e){
+    if(e.name === 'TokenExpiredError'){
+      //토큰의 유효기간이 만료된 경우
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다.',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않는 토큰입니다.',
+    })
   }
 };

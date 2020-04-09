9981c20ffad08c8b46f13ec1f1436b5e433cb21c
Walkers15 <qortmd7777@gmail.com>
2020-04-09 21:43:13 +0900

    10장 따로 이동

--- 10장_nodebird-api/models/index.js
+++ 10장_nodebird-api/models/index.js
@@ -1,23 +1,23 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config')[env];
 const db = {};
 
 const sequelize = new Sequelize(
-    config.database, config.username, config.password, config,
+  config.database, config.username, config.password, config,
 );
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
-db.Hashtag = require('./hashtag')(sequelize,Sequelize);
-//db.Domain = require('../10장_nodebird-api/models/domain')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Domain = require('./domain')(sequelize, Sequelize);
 
 db.User.hasMany(db.Post);
 db.Post.belongsTo(db.User);
-db.Post.belongsToMany(db.Hashtag, { through: 'PostHashTag' });
-db.Hashtag.belongsToMany(db.Post, { through: 'PostHashTag' });
+db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
 db.User.belongsToMany(db.User, {
   foreignKey: 'followingId',
   as: 'Followers',
@@ -28,8 +28,10 @@ db.User.belongsToMany(db.User, {
   as: 'Followings',
   through: 'Follow',
 });
-//db.User.hasMany(db.Domain);
-//db.Domain.belongsTo(db.User);
-console.log(db.User);
-
+db.User.hasMany(db.Domain);
+db.Domain.belongsTo(db.User);
+db.User.findAll({})
+    .then((result)=>{
+      console.log(result);
+    });
 module.exports = db;

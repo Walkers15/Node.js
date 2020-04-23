4815e6dfd42eacb95f732e48d05a40e2d1b16eee
Walkers15 <qortmd7777@gmail.com>
2020-04-23 16:47:09 +0900

    node-place 구글지도 API 추가

--- 13장_node-place/schemas/history.js
+++ 13장_node-place/schemas/history.js
@@ -12,3 +12,5 @@ const historySchema = new Schema({
         default: Date.now,
     },
 });
+
+module.exports = mongoose.model('History',historySchema);
--- 13장_node-place/schemas/index.js
+++ 13장_node-place/schemas/index.js
@@ -9,7 +9,7 @@ module.exports = () => {
             mongoose.set('debug',true);
         }
         mongoose.connect(MONGO_URL,{
-            dbName: 'gifchat',
+            dbName: 'nodeplace',
         },(error)=>{
             if(error){
                 console.log('몽고디비 연결 에러', error);

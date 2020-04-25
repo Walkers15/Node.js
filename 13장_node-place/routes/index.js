3d49a071b70dee9df0d4931470042a525106a934
Walkers15 <qortmd7777@gmail.com>
2020-04-25 19:01:39 +0900

    node-place 완성

--- 13장_node-place/routes/index.js
+++ 13장_node-place/routes/index.js
@@ -3,14 +3,22 @@ const util = require('util');
 const googleMaps = require('@google/maps');
 
 const History = require('../schemas/history');
+const Favorite = require('../schemas/favorite');
 
 const router = express.Router();
 const googleMapsClient = googleMaps.createClient({
     key: process.env.PLACES_API_KEY,
 });
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res, next) => {
+    try{
+        const favorites = await Favorite.find({});
+        console.log(favorites);
+        res.render('index',{ results: favorites, key: process.env.PLACES_API_KEY });
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
 });
 
 router.get('/autocomplete/:query', (req, res, next) => {
@@ -25,7 +33,7 @@ router.get('/autocomplete/:query', (req, res, next) => {
     });
 });
 
-router.get('/search/:query', async (req, res, next) => {
+/*router.get('/search/:query', async (req, res, next) => {
     const googlePlaces = util.promisify(googleMapsClient.places);
     try {
         const history = new History({ query: req.params.query });
@@ -38,10 +46,69 @@ router.get('/search/:query', async (req, res, next) => {
             title: `${req.params.query} 검색 결과`,
             results: response.json.results,
             query: req.params.query,
+            key: process.env.PLACES_API_KEY,
         });
     } catch (error) {
         console.error(error);
         next(error);
+    }
+});*/
+router.get('/search/:query',async(req, res, next)=>{
+    const googlePlaces = util.promisify(googleMapsClient.places);
+    const googlePlacesNearby = util.promisify(googleMapsClient.placesNearby);
+    const { lat, lng, type } = req.query;
+    try{
+        const history = new History({query: req.params.query });
+        await history.save();
+        let response;
+        if(lat && lng){
+            response = await googlePlacesNearby({
+                keyword: req.params.body,
+                location:`${lat},${lng}`,
+                radius: 5000,
+                language: 'ko',
+                type,
+            });
+        }else{
+            response = await googlePlaces({
+                query: req.params.query,
+                language: 'ko',
+                type,
+            });
+        }
+        res.render('result',{
+            title: `${req.params.query} 검색 결과`,
+            results: response.json.results,
+            query: req.params.query,
+            key: process.env.PLACES_API_KEY,
+        });
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/location/:id/favorite', async(req, res, next)=>{
+    try{
+        const result = await Favorite.find({
+            placeId: req.params.id,
+            name: req.body.name,
+            location: [req.body.lng, req.body.lat],
+        });
+        console.log('result: ',result);
+        if(result[0]){
+            console.log('중복');
+            res.send();
+        }else {
+            const favorite = await Favorite.create({
+                placeId: req.params.id,
+                name: req.body.name,
+                location: [req.body.lng, req.body.lat],
+            });
+            res.send(favorite);
+        }
+    }catch(error){
+        console.error(error);
     }
 });
 
